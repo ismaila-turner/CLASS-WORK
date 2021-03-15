a93dfca0aa41a5a1d269894dfe27a74a885dabe3
ismaila turner <64342151+ismaila-turner@users.noreply.github.com>
2021-03-15 01:59:05 +0000

    Add files via upload

---
+++ Classwork.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classwork;
+
+import static java.lang.System.out;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alieu Secka
+ */
+public class Classwork {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      
+      ////  double item1,item2,item3,item4,amount,totalSalary;
+       // System.out.println("please enter item1 amount");
+       // item1 = input.nextDouble();
+       // System.out.println("please enter item2 amount");
+      //  item2 = input.nextDouble();
+      //  System.out.println("please enter item3 amount");
+        //item3 = input.nextDouble();
+        //System.out.println("please enter item4 amount");
+     //   item4 = input.nextDouble();
+      //  amount =item1 + item2 + item3 + item4;
+     //   System.out.println("your total is 50"+amount);
+       // totalSalary = amount*0.09+200;
+       // System.out.println(totalSalary);
+        
+         
+       
+      //  String name,address, phoneNumber,productName;
+        //double productPrice,vat, totalAmount,initialPrice;
+        
+         
+   //     System.out.println("enter your name" );
+     //   name =input.nextLine();
+        
+       // System.out.println("enter your address" );
+//        address =input.nextLine();
+  //      
+    //    System.out.println("enter your phone number " );
+      //  phoneNumber =input.nextLine();
+ //       
+   //     System.out.println("enter your product name " );
+    //    productName =input.nextLine();
+        
+        //System.out.println("enter your productPrice" );
+      //  productPrice =input.nextDouble();
+        
+        //vat=0.15;
+        
+   //     initialPrice= vat*productPrice;
+     //   totalAmount=productPrice+initialPrice;
+        
+       // System.out.println("Name"+name);
+        //System.out.println("Address"+address );
+       // System.out.println("Product Name"+productName );
+       // System.out.println("Product Price"+productPrice );
+       // System.out.println("Vat Amount is"+vat );
+       // System.out.println( "your total Amount after vat is "+totalAmount);
+        
+       
+//        Scanner input=new Scanner(System.in);
+//       String staffName, staffAddress;
+//        double basicSalary,tax=0.4 , taxpay,totalpay;
+//        
+//       System.out.println("please enter your name" );
+//        staffName =input.nextLine();
+//          System.out.println("please enter your address" );
+//        staffAddress =input.nextLine();
+//        
+//         System.out.println("please enter your Basic SALARY" );
+//       basicSalary =input.nextDouble();
+//        
+//    //    System.out.println("please enter your tax paid" );
+//      // tax =input.nextDouble();
+//        
+//        taxpay=basicSalary*0.6;
+//       totalpay=basicSalary-taxpay;
+//        double lessertax = basicSalary * 0.9;
+//        if(basicSalary>=40000){
+//            
+//        System.out.println("you salary is "+basicSalary+ "and you have paid 40 percent tax so your final paid is"+taxpay );
+//       } else {
+//    System.out.println("you salary is"+basicSalary+"and you have paid 10 percent tax so your final paid is " + lessertax);
+//    
+//    
+//    
+//    
+//       }
+
+//double d=4;
+//double e=6;
+//double f=7;
+//while(d>f){
+//d++;
+//e--;
+////b=b+c;
+//
+//
+//}
+//System.out.println(d);
+//System.out.println("this is " +f);
+      
+ 
+//    Scanner input=new Scanner(System.in);
+//    System.out.println("enter a number");
+//    int answer=input.nextInt();
+//    double even=answer%2;
+//    if(even==1){
+//    System.out.println("its odd");
+//    }
+//    else{
+//System.out.println("even");
+//}  
+   ArrayList <String>Array=new ArrayList <String>(); 
+   Array.add("banana");
+   Array.add("orange");
+System.out.println(Array.get(0))  ;
+    }
+    
+}
